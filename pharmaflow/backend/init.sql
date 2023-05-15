a53eabdbb0e04d0d68451d048b91fc6e4307fc57
nehawashikar <63030377+nehawashikar@users.noreply.github.com>
2023-05-15 14:16:25 -0700

    added business rule constraints

--- pharmaflow/backend/init.sql
+++ pharmaflow/backend/init.sql
@@ -1,79 +1,85 @@
-CREATE TABLE CustomerPrescriptionInvoice (
-    CustomerID INT PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Address VARCHAR(255) NOT NULL,
-    PhoneNumber VARCHAR(15),
-    Email VARCHAR(50),
-    DateOfBirth DATE,
-    PrescriptionID INT,
-    PrescriptionQuantity INT,
-    PrescriptionFrequency INT,
-    PrescriptionName VARCHAR(255),
-    InvoiceNumber INT,
-    OrderDate DATE,
-    TotalCost DECIMAL(8,2)
+CREATE TABLE customer_prescription_invoice (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15),
+    email VARCHAR(50),
+    date_of_birth DATE,
+    prescription_id INT,
+    prescription_name VARCHAR(255),
+    prescription_quantity INT,
+    prescription_frequency INT,
+    invoice_number INT,
+    order_date DATE,
+    total_cost DECIMAL(8,2),
+    CHECK (prescription_quantity <= (SELECT quantity_in_stock FROM product WHERE product_number = prescription_id))
 );
 
-CREATE TABLE Employee (
-    EmployeeID INT PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Address VARCHAR(255) NOT NULL,
-    PhoneNumber VARCHAR(15),
-    Email VARCHAR(50),
-    HireDate DATE,
-    PharmacyID INT,
-    Position VARCHAR(50),
-    Salary DECIMAL(8,2)
+CREATE TABLE employee_pharmacy (
+    employee_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15),
+    email VARCHAR(50),
+    hire_date DATE,
+    pharmacy_id INT,
+    position VARCHAR(50),
+    salary DECIMAL(8,2)
 );
 
-CREATE TABLE Product (
-    ProductNumber INT PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Brand VARCHAR(50),
-    Description VARCHAR(255),
-    Category VARCHAR(50),
-    Price DECIMAL(8, 2),
-    QuantityInStock INT NOT NULL,
-    SupplierID INT
+CREATE TABLE product (
+    product_number INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    brand VARCHAR(50),
+    description VARCHAR(255),
+    category VARCHAR(50),
+    price DECIMAL(8, 2),
+    quantity_in_stock INT NOT NULL,
+    supplier_id INT
 );
 
-CREATE TABLE Supplier (
-    SupplierID INT PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Address VARCHAR(255),
-    PhoneNumber VARCHAR(15),
-    Email VARCHAR(50),
-    Website VARCHAR(50)
+CREATE TABLE supplier (
+    supplier_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255),
+    phone_number VARCHAR(15),
+    email VARCHAR(50),
+    website VARCHAR(50)
 );
 
-CREATE TABLE PharmacyOrder (
-    OrderID INT PRIMARY KEY,
-    OrderDate DATE,
-    SupplierID INT,
-    TotalCost DECIMAL(8,2)
+CREATE TABLE pharmacy_order (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    supplier_id INT,
+    total_cost DECIMAL(8,2),
+    CHECK (order_date >= DATEADD(day, (SELECT prescription_frequency FROM customer_prescription_invoice WHERE customer_id = (SELECT customer_id FROM customer_prescription_invoice)), (SELECT MAX(order_date) FROM pharmacy_order WHERE order_id = (SELECT MAX(order_id) FROM pharmacy_order))))
 );
 
-CREATE TABLE OrderLineItem (
-    OrderID INT,
-    ProductNumber INT,
-    Quantity INT,
-    FOREIGN KEY (OrderID) REFERENCES PharmacyOrder(OrderID),
-    FOREIGN KEY (ProductNumber) REFERENCES Product(ProductNumber)
+CREATE TABLE order_line_item (
+    order_id INT,
+    product_number INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES pharmacy_order(order_id),
+    FOREIGN KEY (product_number) REFERENCES product(product_number),
+    CHECK (quantity <= (SELECT quantity_in_stock FROM product WHERE product_number = product_number))
 );
 
-CREATE TABLE InvoiceLineItem (
-    InvoiceNumber INT,
-    ProductNumber INT,
-    QuantityOrdered INT,
-    FOREIGN KEY (InvoiceNumber) REFERENCES CustomerPrescriptionInvoice(InvoiceNumber),
-    FOREIGN KEY (ProductNumber) REFERENCES Product(ProductNumber)
+CREATE TABLE invoice_line_item (
+    invoice_number INT,
+    product_number INT,
+    quantity_ordered INT,
+    FOREIGN KEY (invoice_number) REFERENCES customer_prescription_invoice(invoice_number),
+    FOREIGN KEY (product_number) REFERENCES product(product_number),
+    CHECK (quantity_ordered = (SELECT prescription_quantity FROM customer_prescription_invoice WHERE invoice_number = invoice_number)),
+    CHECK (product_number = (SELECT prescription_id FROM customer_prescription_invoice WHERE invoice_number = invoice_number))
 );
 
-CREATE TABLE Pharmacy (
-    PharmacyID INT PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Address VARCHAR(255),
-    PhoneNumber VARCHAR(15),
-    Email VARCHAR(50),
-    Website VARCHAR(50)
+CREATE TABLE pharmacy (
+    pharmacy_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255),
+    phone_number VARCHAR(15),
+    email VARCHAR(50),
+    website VARCHAR(50),
+    opening_hours VARCHAR(50)
 );
